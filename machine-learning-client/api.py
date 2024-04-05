054329eefe9ecbca7c8de74ced4b765413f1f017
aschwartz01 <adschwartz01@gmail.com>
2024-04-05 16:09:28 -0400

    Adding the basic functionality of the ML for age detection

--- machine-learning-client/api.py
+++ machine-learning-client/api.py
@@ -1,8 +1,9 @@
+"""This module provides functionalities for deep face recognition, specifically age."""
+
 from deepface import DeepFace
 
 print("Hello world")
-result = DeepFace.analyze(img_path = "tester_photo2.png", 
-                          actions=['age','gender'])
+result = DeepFace.analyze(img_path = "tester_photo2.png", actions=['age','gender'])
 
 print("Age: ", result[0]['age'])
 # print("Gender: ", result[0]['dominant_gender'])
