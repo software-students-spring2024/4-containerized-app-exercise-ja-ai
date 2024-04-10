d694e2d042fb23be5d1d4a0f6beab72a361285a3
aschwartz01 <adschwartz01@gmail.com>
2024-04-10 17:49:27 -0400

    Fixing the graph to be a x,y coordinate graph

--- web-app/app.py
+++ web-app/app.py
@@ -170,120 +170,60 @@ def start_processing(image_id):
 
 @app.route("/processing/<image_id>")
 def processing(image_id):
-    """
-    Instead of threading, let's call process_image directly.
-    """
-    try:
-        process_image(image_id)
-        return redirect(url_for("show_results", image_id=image_id))
-    except errors.PyMongoError as e:
-        app.logger.error("Database error during image processing: %s", e)
-        flash("A database error occurred while processing the image.", "error")
-    except requests.exceptions.RequestException as e:
-        app.logger.error("HTTP request error during image processing: %s", e)
-        flash("A network error occurred while processing the image.", "error")
-    except OSError as e:
-        app.logger.error("File handling error during image processing: %s", e)
-        flash("A file handling error occurred while processing the image.", "error")
-    # except Exception as e:
-    #     app.logger.error("An unexpected error occurred during image processing: %s", e)
-    #     flash("An unexpected error occurred while processing the image.", "error")
-    return redirect(url_for("home"))
+    return render_template('processing.html', image_id=image_id)
+
+@app.route('/age_data')
+def age_data():
+    results = list(results_collection.find({}, {"predicted_age": 1, "actual_age": 1}))
+    correct_count = 0
+    incorrect_count = 0
+
+    for result in results:
+        predicted_age = result["predicted_age"]
+        actual_age = int(result["actual_age"])  # Ensure actual ages are integers
+        if abs(predicted_age - actual_age) <= 1:
+            correct_count += 1
+        else:
+            incorrect_count += 1
+
+    aggregated_data = {
+        "correct": correct_count,
+        "incorrect": incorrect_count,
+    }
+    return jsonify(aggregated_data)
+
+@app.route('/age_comparison_data')
+def age_comparison_data():
+    results = list(results_collection.find({}, {"predicted_age": 1, "actual_age": 1}))
+    data = [
+        {
+            'actual_age': int(result["actual_age"]),
+            'predicted_age': result["predicted_age"]
+        } for result in results
+    ]
+    return jsonify(data)
 
 
-def process_image(image_id):
-    """
-    Function that processes the images for upload
+@app.route('/age_distribution')
+def age_distribution():
+    results = list(results_collection.find({}, {"actual_age": 1}))
+    actual_ages = [int(result["actual_age"]) for result in results]
+    
+    # Define your age bins
+    bins = range(0, 101, 10)  # Adjust bins as needed
+    bin_labels = [f"{bins[i]}-{bins[i+1]-1}" for i in range(len(bins)-1)]
+    age_distribution = Counter({label: 0 for label in bin_labels})  # Initialize counter with bins
+    
+    # Count the ages in each bin
+    for age in actual_ages:
+        for i in range(len(bins) - 1):
+            if bins[i] <= age < bins[i+1]:
+                age_distribution[bin_labels[i]] += 1
+                break
 
-    Returns:
-        Nothing. Prints if the image was processed successfully
-    """
-    retry_attempts = 5
-    retry_interval = 5  # seconds
+    return jsonify(dict(age_distribution))
 
-    for attempt in range(retry_attempts):
-        app.logger.info("Attempt %d to process image ID: %s", (attempt + 1), image_id)
-        try:
-            # Attempt to retrieve the image file from GridFS
-            grid_out = fs.get(bson.ObjectId(image_id))
-        except gridfs.errors.NoFile:
-            app.logger.warning("Image not found in GridFS, retrying...")
-            time.sleep(retry_interval)
-            continue
-        image_doc = images_collection.find_one({"image_id": bson.ObjectId(image_id)})
-        if image_doc and image_doc["status"] == "pending":
-            # Proceed with processing if image_doc is valid
-            try:
-                # Retrieve the image file from GridFS
-                grid_out = fs.get(image_doc["image_id"])
-                _, temp_filepath = tempfile.mkstemp()
-                with open(temp_filepath, "wb") as f:
-                    f.write(grid_out.read())
-                app.logger.info("Image written to temporary file: %s", temp_filepath)
-
-                # Call the ML model for processing
-                try:
-                    with open(temp_filepath, "rb") as file:
-                        response = requests.post(
-                            "http://machine-learning-client:5001/analyze",
-                            files={"file": file},
-                            timeout=10,
-                        )
-                    result = response.json()
-                    app.logger.info("Received analysis result: %s", result)
-                finally:
-                    os.remove(temp_filepath)
-                    app.logger.info("Temporary file removed: %s", temp_filepath)
-
-                # Update the database with the analysis results
-                update_result = images_collection.update_one(
-                    {"_id": image_doc["_id"]},
-                    {"$set": {"status": "processed", "analysis": result}},
-                )
-                app.logger.info(
-                    "Image status updated in images_collection. Modified count: %s",
-                    update_result.modified_count,
-                )
-
-                # Insert the result into the results_collection
-                insert_result = results_collection.insert_one(
-                    {
-                        "image_id": image_doc["image_id"],
-                        "filename": image_doc["filename"],
-                        "analysis": result,
-                        "upload_date": image_doc["upload_date"],
-                    }
-                )
-                app.logger.info(
-                    "Result inserted into results_collection with ID: %s",
-                    insert_result.inserted_id,
-                )
-                return
-            except errors.PyMongoError as e:
-                app.logger.error("MongoDB operation failed: %s", e)
-                # handle MongoDB specific logic here
-            except Exception as e:
-                app.logger.error("Unexpected error occurred: %s", e)
-                traceback.print_exc()
-            finally:
-                # Set status to "failed" to indicate processing did not complete
-                images_collection.update_one(
-                    {"_id": bson.ObjectId(image_id)}, {"$set": {"status": "failed"}}
-                )
-                app.logger.info(
-                    "Image status updated to 'failed' for image ID: %s", image_id
-                )
-        if attempt < retry_attempts - 1:
-            time.sleep(retry_interval)
-    # got rid of else for pylinting. If necessary, add it back in
-    app.logger.error(
-        "No image found for image ID: %s after %s attempts.",
-        image_id,
-        retry_attempts,
-    )
-
-
-@app.route("/check_status/<image_id>")
+@app.route('/check_status/<image_id>')
 def check_status(image_id):
     """
     Function that checks the status of the images being processed
@@ -310,29 +250,25 @@ def show_results(image_id):
     try:
         image_id = bson.ObjectId(image_id)
     except bson.errors.InvalidId:
-        flash("Invalid image ID.", "error")
-        return redirect(url_for("home"))
-    result = results_collection.find_one({"image_id": image_id}, {"_id": 0})
+        return "Invalid image ID", 400
+    result = results_collection.find_one({"image_id": image_id}, {'_id': 0})
     if result:
         try:
             fs_image = fs.get(image_id)
-            result["image_data"] = base64.b64encode(fs_image.read()).decode("utf-8")
-            # Assuming `result["analysis"]` contains "age" and "gender"
-            result.update(result["analysis"])
-        except gridfs.errors.NoFile:
-            flash("File not found in database.", "error")
-            return redirect(url_for("home"))
-        except KeyError as e:
-            flash(f"Key error in result analysis: {str(e)}", "error")
-            return redirect(url_for("home"))
-        except gridfs.errors.GridFSError as e:
-            flash(f"Error accessing file in GridFS: {str(e)}", "error")
-            return redirect(url_for("home"))
-        return render_template(
-            "results.html", result=result, filename=result["filename"]
-        )
-    flash("Result not found.", "error")
-    return redirect(url_for("home"))
+            result['image_data'] = base64.b64encode(fs_image.read()).decode('utf-8')
+        except:
+            result['image_data'] = None
+
+        # Calculate correctness here and add it to the result
+        predicted_age = result.get('predicted_age')
+        actual_age = int(result.get('actual_age', 0))  # Ensure actual_age is an integer
+        result['correct'] = abs(predicted_age - actual_age) <= 1
+
+        return render_template('results.html', results=[result])
+    else:
+        flash('Result not found.', 'error')
+        return redirect(url_for('home'))
+
 
 
 if __name__ == "__main__":
