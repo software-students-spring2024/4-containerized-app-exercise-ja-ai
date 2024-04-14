4f58d2e0242450200d7d2f258f447e7f0a1371d7
ak8000 <ak8000@nyu.edu>
2024-04-14 18:26:36 -0400

    working processing setup

--- machine-learning-client/api.py
+++ machine-learning-client/api.py
@@ -3,7 +3,10 @@ API module for image analysis using the DeepFace library.
 This module provides functionalities to analyze images for age.
 """
 
+import logging
 from deepface import DeepFace
+
+logging.basicConfig(level=logging.INFO)
 
 
 def analyze_image(img_path):
@@ -11,10 +14,16 @@ def analyze_image(img_path):
     Analyze an image for age and gender using DeepFace.
 
     Parameters:
-    - img_path (str): Path to image file
+        img_path (str): Path to the image file
 
     Returns:
-    - list: Analysis results including age and gender
+        list: Analysis results including age and gender
     """
-    result = DeepFace.analyze(img_path=img_path, actions=["age", "gender"])
-    return [result[0]]
+    try:
+        logging.info("Analyzing the image at path: %s", img_path)
+        result = DeepFace.analyze(img_path=img_path, actions=["age", "gender"])
+        logging.info("Analysis result: %s", result)
+        return [result[0]["age"], result[0]["gender"]]
+    except Exception as e:
+        logging.error("An error occurred during image analysis: %s", e)
+        raise
