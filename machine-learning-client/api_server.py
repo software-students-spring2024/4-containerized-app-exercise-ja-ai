a95976960ec43e8b6c57178a913e6fb0d9b8fdc6
aschwartz01 <adschwartz01@gmail.com>
2024-04-14 11:51:32 -0400

    fixing the linting on api_server.py

--- machine-learning-client/api_server.py
+++ machine-learning-client/api_server.py
@@ -27,6 +27,7 @@ def analyze():
         result = analyze_image(path)
         os.remove(path)
         return jsonify(result)
+    return jsonify({"error":"Unknown error"}), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001, debug=True)
