e5f189b880bb137f3aa71e05e84ea9a6bcd733df
iok206 <iok206@nyu.edu>
2024-04-16 18:37:55 -0400

    moved around test files and removed previous test file

--- web-app/tests/test_app.py
+++ web-app/tests/test_app.py
@@ -1,5 +1,13 @@
 import pytest
-from app import app, allowed_file
+import os
+import threading
+import time
+from bson import ObjectId
+from unittest.mock import MagicMock
+from app import app, allowed_file, process_task
+
+
+IMAGE_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "test_png", "tester_photo.png"))
 
 @pytest.fixture()
 def testing_app():
@@ -19,6 +27,14 @@ def client(testing_app):
     """
     return testing_app.test_client()
 
+@pytest.fixture()
+def task_queue():
+    """
+    Test queue for tasks
+    """
+    test_queue = []
+    yield test_queue
+    
 def test_allowed_file():
     # Tests cases: allowed file types
     print("Testing allowed_file function...")
@@ -31,6 +47,46 @@ def test_allowed_file():
     print("All allowed_file tests passed.")
 
 def test_home(client):
+    # Test loading home page
     response = client.get('/')
     assert response.status_code == 200
+
+def test_upload_image(client):
     
+    # Test loading upload page
+    response = client.get('/upload')
+    assert response.status_code == 200
+
+    # Test post request
+    # Using 'with' ensures the file is open only during the operation
+    with open(IMAGE_PATH, "rb") as image_file:
+        # Define data and files properly
+        data = {
+            "age": (None, "30"),  # This is how you normally send non-file fields
+            "image": (image_file, "tester_photo.png")  # Ensuring the file tuple is correct
+        }
+        # Flask test client handles content-type automatically here
+        response = client.post("/upload", data=data)
+    assert response.status_code == 400
+
+# Test get response for task
+def test_start_task(client):
+    data = {"task_id": "12345"}
+    response = client.post("/start_task", json=data)
+    assert response.status_code == 202
+
+# Test make sure task gets passed
+def test_process_task(task_queue):
+    task_queue.append("12345")
+    threading.Thread(target=process_task, daemon=True).start()
+    time.sleep(1)
+    assert task_queue == ["12345"]
+
+def test_get_result(client):
+    task_id = "12345"
+    response = client.get(f"/get_result/{task_id}")
+    assert response.status_code == 202
+
+    data = response.json
+    assert data["task_id"] == task_id
+    assert data["status" ] == "Processing"
